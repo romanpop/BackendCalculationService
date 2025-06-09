8a0df481445d1b3ced04de494e6c0c9ed3892279
Roman Pop <roman.pop@gmail.com>
2025-06-09 13:46:34 +0300

    1

--- Controllers/WorkbookController.cs
+++ Controllers/WorkbookController.cs
@@ -3,6 +3,7 @@ using BackendCalculationService.Services;
 using BackendCalculationService.Models;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace BackendCalculationService.Controllers;
 
@@ -22,14 +23,15 @@ public class WorkbookController : ControllerBase
     [HttpPost("upload")]
     public async Task<IActionResult> Upload(IFormFile file)
     {
-        var path = await _storageService.SaveAsync(file);
+        var path = await _storageService.SaveAsync(file).ConfigureAwait(false);
         return Ok(new { FilePath = path });
     }
 
     [HttpPost("calculate")]
     public async Task<IActionResult> Calculate([FromBody] CalculationRequest req)
     {
-        var outputs = await _calcService.CalculateAsync(req.FilePath, req.Inputs);
+        var inputs = req.Inputs ?? new Dictionary<string, object>();
+        var outputs = await _calcService.CalculateAsync(req.FilePath, inputs);
         return Ok(outputs);
     }
 }
